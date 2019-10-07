866e72ef54c073ee7d6795b1368633bc3b272b76
Huda Yousif <hjdoski@gmail.com>
2019-10-07 18:32:07 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,25 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def greeting
+  puts "Hello World"
+end
+
+greeting => Hello World
+
+def greeting
+  puts "Hello World"
+end
+ 
+greeting
+greeting
+greeting
+greeting
+greeting
+
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
